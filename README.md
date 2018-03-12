05f0e19eaa1725db42e865e25e335d0b203db246
kenleytomlin <kenleytomlin@gmail.com>
2018-03-12 10:49:34 +0000

    feat(index.js): Initialize the git repo and first working prototype

---
+++ README.md
@@ -0,0 +1,5 @@
+## Serverless rbdms migrations
+
+This plugin will run a function named 'up' after a deploy is complete.
+On rollback it will run a function named 'down' after a rollback is
+complete. This module is a work in progress. More details to follow.
---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.log = message =>
+      serverless.cli.log.bind(serverless.cli)(`Migrations - ${message}`);
+
+    this.commands = {
+      migrate: {
+        usage: 'Runs your database migrations',
+        commands: {
+          up: {
+            usage: 'Runs forward migrations',
+            lifecycleEvents: ['up'],
+            options: {
+              function: {
+                usage: 'The name of the function that will perform the migration defaults to up',
+                shortcut: 'f',
+                default: 'up'
+              }
+            }
+          },
+          down: {
+            usage: 'Rolls back migrations',
+            lifecycleEvents: ['down'],
+            options: {
+              function: {
+                usage: 'The name of the function that will perform the rollback defaults to down',
+                shortcut: 'f',
+                default: 'down'
+              }
+            }
+          }
+        }
+      }
+    };
+
+    this.hooks = {
+      'after:deploy:deploy': this.migrate.bind(this),
+      'after:rollback:rollback': this.rollback.bind(this),
+      'migrate:down': this.rollback.bind(this),
+      'migrate:up': this.afterDeploy.bind(this)
+    };
+  }
+
+  migrate () {
+    this.options.function = 'up';
+    this.log(`Running migration function ${this.options.function}`);
+
+    return this.serverless.pluginManager.spawn('invoke',{
+      terminateLifecycleAfterExecution: true
+    }).then(() => {
+      this.log('Migrations complete');
+    });
+  }
+
+  rollback () {
+    this.options.function = 'down';
+    this.log(`Running rollback function ${this.options.function}`);
+    return this.serverless.pluginManager.spawn('invoke', {
+      terminateLifecycleAfterExecution: true
+    })
+    .then(() => {
+      this.log('Rollback complete');
+    });
+  }
+}
+
+module.exports = ServerlessPlugin;
